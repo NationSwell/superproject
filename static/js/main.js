dc835ba7623fc8336f14517e3b138534656e3d18
David Chapman <david@ronikdesign.com>
2013-10-31 17:34:25 -0400

    - continuing to style homepage - carousels - list module ip

--- static/js/main.js
+++ static/js/main.js
@@ -3,4 +3,35 @@ $(function() {
     $("[for='search']").on("click", function() {
         $('#search').toggleClass('open');
     });
+
+    // init slideshows
+    $(".carousel").each(function(){
+        var $this = $(this),
+            $carousel = $this.find(".carousel__items");
+
+        var isPhotoStory = $this.hasClass('carousel--photo');
+
+        $carousel.carouFredSel({
+            responsive: true,
+            width: '100%',
+            transition: true,
+            items: {
+                visible: isPhotoStory ? 3 : 1,
+                start: isPhotoStory ? -1 : 1
+            },
+            scroll: {
+                items: 1,
+                duration: 300
+            },
+            auto: {
+                play: false
+            },
+            prev: {
+                button: $this.find('.carousel__control--prev')
+            },
+            next: {
+                button: $this.find('.carousel__control--next')
+            }
+        });
+    });
 });
