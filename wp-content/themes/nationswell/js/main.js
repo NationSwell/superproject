4547300523b41e0d64b6c9ebafaf509e2d19aec3
Mark Catalano <mark@ronikdesign.com>
2013-10-31 20:17:44 -0400

    - stubbing out nationswell view twig integration

---
+++ wp-content/themes/nationswell/js/main.js
@@ -0,0 +1,41 @@
+(function($){
+    $(function() {
+        // expand/collapse header search field
+        $("[for='search']").on("click", function() {
+            $('#search').toggleClass('open');
+        });
+
+        // init slideshows
+        $(".carousel").each(function(){
+            var $this = $(this),
+                $carousel = $this.find(".carousel__items");
+
+            var isPhotoStory = $this.hasClass('carousel--photo');
+
+            console.log($.fn.jquery);
+
+            $carousel.carouFredSel({
+                responsive: isPhotoStory ? false : true,
+                width: '100%',
+                transition: true,
+                items: {
+                    visible: isPhotoStory ? 3 : 1,
+                    start: isPhotoStory ? -1 : 1
+                },
+                scroll: {
+                    items: 1,
+                    duration: 300
+                },
+                auto: {
+                    play: false
+                },
+                prev: {
+                    button: $this.find('.carousel__control--prev')
+                },
+                next: {
+                    button: $this.find('.carousel__control--next')
+                }
+            });
+        });
+    });
+})(jQuery);
