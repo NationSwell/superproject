e63670044ff331c52a9e4ff80814b2ad07b69f84
Mark Catalano <mark@ronikdesign.com>
2013-11-13 16:17:00 -0500

    - move stuff out of functions.php into separate files. - Start in on the sidebar - add chartbeat plugin

---
+++ wp-content/themes/nationswell/menu.php
@@ -0,0 +1,16 @@
+<?php
+// Register Navigation Menus
+function custom_navigation_menus()
+{
+    register_nav_menus(
+        array(
+            'menu_main' => 'Main Menu',
+            'menu_footer' => 'Footer Menu',
+            'menu_topic' => 'Topic Menu',
+        )
+    );
+}
+
+// Hook into the 'init' action
+add_action('init', 'custom_navigation_menus');
+?>
