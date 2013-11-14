fa98503bb6ca9068c002fc83496f67bd58fe91ef
Andrew Sprouse <sprouse.a@gmail.com>
2013-11-14 15:58:37 -0500

    cleanup

--- wp-content/themes/nationswell/lib/classes/NationSwellPost.php
+++ wp-content/themes/nationswell/lib/classes/NationSwellPost.php
@@ -43,16 +43,13 @@ if (class_exists('TimberPost')) {
         }
 
         function facebook_share_url(){
-
-            $fblink = 'https://www.facebook.com/sharer/sharer.php?u=' . urlencode($this->permalink()) .'&title=' . urlencode($this->title());
-
-            return $fblink;
+            return 'https://www.facebook.com/sharer/sharer.php?u='
+            . urlencode($this->permalink()) .'&title=' . urlencode($this->title());
         }
 
         function twitter_share_url(){
-            $tweetlink = 'https://twitter.com/share?url=' . urlencode($this->permalink()) . '&text=' . urlencode($this->title()) . '&via=nationswell';
-
-            return $tweetlink;
+            return 'https://twitter.com/share?url='
+            . urlencode($this->permalink()) . '&text=' . urlencode($this->title()) . '&via=nationswell';
         }
     }
 }
