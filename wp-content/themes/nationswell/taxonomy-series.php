9a07dec71ef6e20bf5c0d3c1f07c32a52d01cb26
Andrew Sprouse <sprouse.a@gmail.com>
2013-11-18 15:02:15 -0500

    Wired up the title and count to series.twig

--- wp-content/themes/nationswell/taxonomy-series.php
+++ wp-content/themes/nationswell/taxonomy-series.php
@@ -8,7 +8,16 @@
  * @subpackage  Timber
  * @since    Timber 0.1
  */
+global $wp_query;
+
 $data = Timber::get_context();
+
+$term = get_term_by( 'slug', get_query_var( 'term' ), get_query_var( 'taxonomy' ));
+
+$data['series'] = array(
+    'name' => $term->name,
+    'count' => $wp_query->found_posts
+);
 $data['posts'] = Timber::get_posts();
 
 Timber::render('series.twig', $data);
