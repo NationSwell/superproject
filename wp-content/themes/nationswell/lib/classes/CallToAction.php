f3723d1c626b22485021e684a5da3220e75ce827
Andrew Sprouse <sprouse.a@gmail.com>
2013-11-22 16:34:45 -0500

    - Added image function to CallToAction class - Added proper usage of the image function to the freeform and donate calls to action

--- wp-content/themes/nationswell/lib/classes/CallToAction.php
+++ wp-content/themes/nationswell/lib/classes/CallToAction.php
@@ -3,12 +3,25 @@ if (class_exists('TimberPost')) {
     class CallToAction extends TimberPost {
         private $petition;
 
+        function __construct($pid = null) {
+            parent::__construct($pid);
+
+            // remove the image property and use the acf function instead;
+            if(isset($this->image)) {
+                unset($this->image);
+            }
+        }
+
         public function petition() {
             if(!isset($this->petition)) {
                 $this->petition = $this->type === 'petition' ? new ChangeOrgPetition($this->ID) : false;
             }
 
             return $this->petition;
+        }
+
+        public function image() {
+            return get_field('image', $this->ID);
         }
     }
 }
