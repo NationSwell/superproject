e2a8a27c446326c59d96bd1972333ec73692bb73
Mark <mark@ronikdesign.com>
2013-11-27 15:17:36 -0500

    check for empty so we dont' blow up the page

--- wp-content/themes/nationswell/lib/widgets/widget-stories.php
+++ wp-content/themes/nationswell/lib/widgets/widget-stories.php
@@ -4,32 +4,39 @@ add_action('widgets_init', 'stories_widget');
 
 function stories_widget()
 {
-    register_widget( 'Stories_Widget' );
+    register_widget('Stories_Widget');
 }
 
-class Stories_Widget extends WP_Widget {
+class Stories_Widget extends WP_Widget
+{
 
-    function Stories_Widget() {
+    function Stories_Widget()
+    {
         $widget_ops = array('classname' => 'stories', 'description' => 'NationSwell Widget for displaying a list of stories');
 
         $control_ops = array('id_base' => 'stories-widget');
 
-        $this->WP_Widget( 'stories-widget', 'NationSwell Stories List Widget', $widget_ops, $control_ops );
+        $this->WP_Widget('stories-widget', 'NationSwell Stories List Widget', $widget_ops, $control_ops);
     }
 
 
-    function widget( $args, $instance) {
-        extract( $args );
+    function widget($args, $instance)
+    {
+        extract($args);
 
-        //Our variables from the widget settings.
-        $context = array();
-        $post_id = intval($instance['post_id']);
+        if (!empty($instance['post_id'])) {
 
-        $post = Timber::get_post(array($post_id));
-        $context['header'] = $post->post_title;
+            //Our variables from the widget settings.
+            $context = array();
+            $post_id = intval($instance['post_id']);
 
-        $posts = get_field('story_list', $post->ID);
-        $context['posts'] = Timber::get_posts($posts, 'NationSwellPost');
+            $post = Timber::get_post(array($post_id));
+            $context['header'] = $post->post_title;
+
+            $posts = get_field('story_list', $post->ID);
+            $context['posts'] = Timber::get_posts($posts, 'NationSwellPost');
+
+        }
 
         $template = array('widget-stories.twig', 'empty.twig');
 
@@ -46,24 +53,28 @@ class Stories_Widget extends WP_Widget {
     }
 
     //Update the widget
-    function update( $new_instance, $old_instance ) {
+    function update($new_instance, $old_instance)
+    {
         $instance = $old_instance;
 
         //Strip tags from title and name to remove HTML
-        $instance['post_id'] = strip_tags( $new_instance['post_id'] );
+        $instance['post_id'] = strip_tags($new_instance['post_id']);
 
         return $instance;
     }
 
-    function form( $instance ) {
+    function form($instance)
+    {
 
         //Set up some default widget settings.
-        $defaults = array( 'post_id' => '');
-        $instance = wp_parse_args( (array) $instance, $defaults ); ?>
+        $defaults = array('post_id' => '');
+        $instance = wp_parse_args((array)$instance, $defaults); ?>
 
         <p>
-            <label for="<?php echo $this->get_field_id( 'post_id' ); ?>"><?php _e('Post ID:', 'ID'); ?></label>
-            <input id="<?php echo $this->get_field_id( 'post_id' ); ?>" name="<?php echo $this->get_field_name( 'post_id' ); ?>" value="<?php echo $instance['post_id']; ?>" style="width:100%;" />
+            <label for="<?php echo $this->get_field_id('post_id'); ?>"><?php _e('Post ID:', 'ID'); ?></label>
+            <input id="<?php echo $this->get_field_id('post_id'); ?>"
+                   name="<?php echo $this->get_field_name('post_id'); ?>" value="<?php echo $instance['post_id']; ?>"
+                   style="width:100%;"/>
         </p>
 
     <?php
