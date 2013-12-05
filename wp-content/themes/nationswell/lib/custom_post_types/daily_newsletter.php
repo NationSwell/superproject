633e62f2fcec50678b5ae171a439e853a2926365
Mark <mark@ronikdesign.com>
2013-12-05 16:07:45 -0500

    - daily newsletters work with slug permalinks

--- wp-content/themes/nationswell/lib/custom_post_types/daily_newsletter.php
+++ wp-content/themes/nationswell/lib/custom_post_types/daily_newsletter.php
@@ -1,6 +1,5 @@
 <?php
 
-add_action('init', 'create_daily_newsletter_post_type');
 
 function create_daily_newsletter_post_type()
 {
@@ -18,18 +17,9 @@ function create_daily_newsletter_post_type()
             'rewrite' => array( 'slug' => 'daily-newsletter' ),
         )
     );
-}
 
-function my_rewrite_flush() {
-    // First, we "add" the custom post type via the above written function.
-    // Note: "add" is written with quotes, as CPTs don't get added to the DB,
-    // They are only referenced in the post_type column with a post entry,
-    // when you add a post of this CPT.
-    my_cpt_init();
-
-    // ATTENTION: This is *only* done during plugin activation hook in this example!
-    // You should *NEVER EVER* do this on every page load!!
     flush_rewrite_rules();
 }
 
-add_action( 'after_switch_theme', 'my_rewrite_flush' );
+//add_action( 'after_switch_theme', 'create_daily_newsletter_post_type' );
+add_action('init', 'create_daily_newsletter_post_type');
