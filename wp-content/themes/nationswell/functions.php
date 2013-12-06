079a0230a8e68660f1c480033cab44fcb1465d87
Mark Catalano <mark@ronikdesign.com>
2013-12-06 13:57:40 -0500

    - truncate long autor bios in story page author box

--- wp-content/themes/nationswell/functions.php
+++ wp-content/themes/nationswell/functions.php
@@ -215,6 +215,30 @@ if (!function_exists('http_response_code'))
     }
 }
 
+function truncate($input, $maxWords, $maxChars)
+{
+    $words = preg_split('/\s+/', $input);
+    $words = array_slice($words, 0, $maxWords);
+    $words = array_reverse($words);
+
+    $chars = 0;
+    $truncated = array();
+
+    while(count($words) > 0)
+    {
+        $fragment = trim(array_pop($words));
+        $chars += strlen($fragment);
+
+        if($chars > $maxChars) break;
+
+        $truncated[] = $fragment;
+    }
+
+    $result = implode($truncated, ' ');
+
+    return $result . ($input == $result ? '' : '...');
+}
+
 
 // Change.org
 global $change_org_api;
