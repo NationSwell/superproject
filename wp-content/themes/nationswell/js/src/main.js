cfc25bb806dee62f0e71f3214c1e4db36894878c
David Chapman <david@ronikdesign.com>
2013-12-17 11:31:10 -0500

    - fix for disappearing indicators on desktop-mobile size switch

--- wp-content/themes/nationswell/js/src/main.js
+++ wp-content/themes/nationswell/js/src/main.js
@@ -277,7 +277,7 @@
                         if (isPeek) {
                             var $activeSlide = isFullPeek ? items.filter(":eq(1)") : items.filter(":eq(0)"),
                                 $activeCaption = $activeSlide.find('.carousel-item__title > span').text(),
-                                $activeIndicator = $activeSlide.find('.indicator').detach().removeClass('hide_mobile'),
+                                $activeIndicator = $activeSlide.find('.indicator').clone().removeClass('hide_mobile'),
                                 $externalContainerLink = $externalContainer.find('.link-wrapper'),
                                 activeHref = $activeSlide.find('.link-wrapper').attr('href');
 
