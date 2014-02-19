f78fbf61d54e6b1004129470a7b45af38e4892e8
taytorious <taylor.johnson404@gmail.com>
2014-02-19 15:08:29 -0500

    Update subscribesheet.php
    Uses the ZendGData library to store subscriber's e-mail to the shared Google Spreadsheet.

--- subscribesheet.php
+++ subscribesheet.php
@@ -1,7 +1,28 @@
 <?php
-
-$Email = $_POST['email'];
-
-exec("java -jar subscribeofficial.jar " . $Email, $success);
-print_r($success);
+ 
+ $Email = $_POST[email]
+ $timestmp = date('l jS \of F Y h:i:s A');
+// Zend library include path
+set_include_path(get_include_path() . PATH_SEPARATOR . "$_SERVER[DOCUMENT_ROOT]/ZendGdata-1.8.1/library");
+     
+include_once("Google_Spreadsheet.php");
+ 
+$u = "username@gmail.com";
+$p = "password";
+ 
+$ss = new Google_Spreadsheet($u,$p);
+$ss->useSpreadsheet("Newsletter Signup");
+ 
+// if not setting worksheet, "Sheet1" is assumed
+// $ss->useWorksheet("worksheetName");
+ 
+$row = array
+(
+    "date" => $timestmp
+    , "email" => $Email
+);
+ 
+if ($ss->addRow($row)) echo "You are now subscribed to the Newsletter!";
+else echo "Error, unable to add you to the newsletter subscriptions.";
+ 
 ?>
