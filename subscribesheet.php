da04a7b79fd1e240d95b3fb28806d79daab63f81
taytorious <taylor.johnson404@gmail.com>
2014-02-19 15:29:50 -0500

    Update subscribesheet.php

--- subscribesheet.php
+++ subscribesheet.php
@@ -3,7 +3,7 @@
  $Email = $_POST[email]
  $timestmp = date('l jS \of F Y h:i:s A');
 // Zend library include path
-set_include_path(get_include_path() . PATH_SEPARATOR . "$_SERVER[DOCUMENT_ROOT]/ZendGdata-1.8.1/library");
+set_include_path(get_include_path() . PATH_SEPARATOR . "$_SERVER[DOCUMENT_ROOT]/ZendGdata-2.2.5/library");
      
 include_once("Google_Spreadsheet.php");
  
