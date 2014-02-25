efeec4cbc9d33e2e4ad5d35d3ee5c68307338d0a
taytorious <taylor.johnson404@gmail.com>
2014-02-25 16:40:22 -0500

    Update subscribesheet.php
    Updated to submit e-mail subscribers to the MailChimp list with the double opt-in turned OFF. Uses MailChimp API 2.0 and the subscribe function. Data center set to us6. API Key and List ID obtained from MailChimp account page.

--- subscribesheet.php
+++ subscribesheet.php
@@ -1,44 +1,39 @@
 <?php
  
-   include_once("Google_Spreadsheet.php");
+$email = _POST['Email'];
 
-   $user = 'USER';			//need to create dummy acct
-   $pass = 'PASSWD';						
+$listId = "8eaa257d1b";
+$apikey = "99983ece6b5ad94f7c4f026238381f4d-us6"; 
+$double_optin=false;
+$update_existing=false;
+$send_welcome=false;
+$email_type = 'html';            
+$data = array(
+        'email'=>$email,
+        'apikey'=>$apikey,
+        'id' => $listId,
+        'double_optin' => $double_optin,
+        'update_existing' => $update_existing,
+        'send_welcome' => $send_welcome,
+        'email_type' => $email_type
+    );
+$payload = json_encode($data);
+ 
 
-   $ss = new Google_Spreadsheet($user, $pass);
-   $ss->useWorksheet("Sheet1");						//need to set these to match spreadsheet
-   $ss->useSpreadsheet("Newsletter Subscription");
-
-
-
-
-
-$email = $_POST['email'];
-$timestmp = date('l jS \of F Y h:i:s A');
-
-
-
-
-
-
-$rowData = array
-(
-	  "timestamp" => $timestmp
-    , "email" => $email
-
-);
-
-if ($ss->addRow($rowData)) {
-            // Display success page here
-
-    echo "Thanks for subscribing to thenewsletter!";
-
+$submit_url = "http://us6.api.mailchimp.com/2.0/?method=subscribe";
+ 
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $submit_url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, urlencode($payload));
+ 
+$result = curl_exec($ch);
+curl_close ($ch);
+$data = json_decode($result);
+if ($data->error){
+    echo $data->code .' : '.$data->error."\n";
 } else {
-            // Failed to write to the spreadsheet
-    echo "Sorry there was an error processing your request.";
+    echo "Thank you for subscribing to our newsletter!\n";
 }
-
- 
-
- 
-?>	
+?> 
