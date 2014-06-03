a5da706917a26f4adc455c5c63473fda73f0c5b4
Taylor Johnson <taylor.johnson404@gmail.com>
2014-06-03 15:40:27 -0400

    Added UTM URL builder to theme.

---
+++ wp-content/themes/nationswell/utm/utmbuilder.js
@@ -0,0 +1,22 @@
+var d = new Date();
+var month = d.getMonth()+1;
+var day = d.getDate();
+var year = d.getFullYear()-2000;
+
+$(document).ready(function() {
+	$("#facebook").click(function() {
+		$("#outputurl").val($("#inputurl").val() + "?utm_source=facebook&utm_medium=nsfbpage");
+	});
+	
+	$("#twitter").click(function() {
+		$("#outputurl").val($("#inputurl").val() + "?utm_source=twitter&utm_medium=nstwitter");
+	});
+
+	$("#outreach").click(function() {
+		$("#outputurl").val($("#inputurl").val() + "?utm_source=outreach&utm_campaign=nsO" + month + day + year);
+	});
+	
+	$("#buttonrow").click(function() {
+		$("#outputurl").select();
+	});
+});
