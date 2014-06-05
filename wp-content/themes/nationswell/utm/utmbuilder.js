d6aa9e1ffd1113367ae08cc325225e23a9f5f621
Taylor Johnson <taylor.johnson404@gmail.com>
2014-06-05 08:36:04 -0400

    Added UTM builder page template

--- wp-content/themes/nationswell/utm/utmbuilder.js
+++ wp-content/themes/nationswell/utm/utmbuilder.js
@@ -1,22 +1,24 @@
-var d = new Date();
-var month = d.getMonth()+1;
-var day = d.getDate();
-var year = d.getFullYear()-2000;
-
-$(document).ready(function() {
-	$("#facebook").click(function() {
-		$("#outputurl").val($("#inputurl").val() + "?utm_source=facebook&utm_medium=nsfbpage");
-	});
+(function ($) {
+	var d = new Date();
+	var month = d.getMonth()+1;
+	var day = d.getDate();
+	var year = d.getFullYear()-2000;
 	
-	$("#twitter").click(function() {
-		$("#outputurl").val($("#inputurl").val() + "?utm_source=twitter&utm_medium=nstwitter");
-	});
-
-	$("#outreach").click(function() {
-		$("#outputurl").val($("#inputurl").val() + "?utm_source=outreach&utm_campaign=nsO" + month + day + year);
-	});
+	$(document).ready(function() {
+		$("#facebook").click(function() {
+			$("#outputurl").val($("#inputurl").val() + "?utm_source=facebook&utm_medium=nsfbpage");
+		});
+		
+		$("#twitter").click(function() {
+			$("#outputurl").val($("#inputurl").val() + "?utm_source=twitter&utm_medium=nstwitter");
+		});
 	
-	$("#buttonrow").click(function() {
-		$("#outputurl").select();
+		$("#outreach").click(function() {
+			$("#outputurl").val($("#inputurl").val() + "?utm_source=outreach&utm_campaign=nsO" + month + day + year);
+		});
+		
+		$("#buttonrow").click(function() {
+			$("#outputurl").select();
+		});
 	});
-});
+})(jQuery);
--- wp-content/themes/nationswell/utmbuilder.php
+++ wp-content/themes/nationswell/utmbuilder.php
@@ -1,13 +1,19 @@
+<?php 
+/*
+Template Name: UTM Builder
+ */
+?>
 <!DOCTYPE html>
 
 <head>
 	<link href='http://fonts.googleapis.com/css?family=Gudea:400,700,400italic|Oswald:400,700|Montserrat:400,700|Noto+Serif:400,700'
       rel='stylesheet' type='text/css'>
-<link rel="stylesheet" type="text/css" href="utmstyles.css">
+<link rel="stylesheet" type="text/css" href="http://nationswell.com/wp-content/themes/nationswell/utm/utmstyles.css">
 <script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
 <script src="http://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
-<script type="text/javascript" src="utmbuilder.js"></script>
+<script type="text/javascript" src="http://nationswell.com/wp-content/themes/nationswell/utm/utmbuilder.js"></script>
 </head>
+
 
 <body>
 
