416356b73c0373124961da681e9625f6cb64ecee
Taylor Johnson <taylor.johnson404@gmail.com>
2014-06-09 14:52:13 -0400

    Added 'load moreposts' functionality to seach queries.

--- wp-content/themes/nationswell/search.php
+++ wp-content/themes/nationswell/search.php
@@ -14,15 +14,14 @@
 
 	$templates = array('search.twig', 'archive.twig', 'index.twig');
 	$data = Timber::get_context();
-	$author_found = null;
-	if ($wp_query->is_author)
-	{
-		$author_found = $wp_query->queried_object;
-	}
+	$page = max($wp_query->query_vars['paged'], 1);
+	$data['more'] = $wp_query->max_num_pages > $page ? get_next_page_link($page+1) : false;
 	$data['author'] = $author_found;
     $data['total_results'] = $wp_query->found_posts;
 	$data['title'] = 'Search results for '. get_search_query();
 	$data['posts'] = Timber::get_posts(array(), 'NationSwellPost');
     $data['sidebar_static'] = Timber::get_widgets('sidebar_static');
+    
+    $more = isset($_GET['ajax-more']);
 	
-	Timber::render($templates, $data);
+	Timber::render($more ? 'search-more.twig' : $templates, $data);
