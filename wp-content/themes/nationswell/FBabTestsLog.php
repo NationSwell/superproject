19859e4656f473862bd1fc3c5d7952420f0de642
Taylor Johnson <taylor.johnson404@gmail.com>
2014-07-04 12:24:30 -0400

    FB AB tester app.

---
+++ wp-content/themes/nationswell/FBabTestsLog.php
@@ -0,0 +1,22 @@
+<?php
+/*
+Template Name: FBAB Tester
+ */
+?>
+
+<!DOCTYPE html>
+<meta charset="utf-8">
+<script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+<script src="http://d3js.org/d3.v3.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/tabletop.js/1.1.0/tabletop.min.js"></script>
+<link rel="stylesheet" type="text/css" href="http://nationswell.com/wp-content/themes/nationswell/FBABTestLog/FBabTestsStyle.css">
+
+<body>
+<div id="topnavspacer"><h1>NationSwell Facebook A/B Testing Results</h1></div>
+<div id="topnav">
+    <h1>NationSwell Facebook A/B Testing Results</h1>
+</div>
+</div>
+<div id="datadrop"></div>
+<script src="http://nationswell.com/wp-content/themes/nationswell/FBABTestLog/FBabTestDataSort.js" type="text/javascript"></script>
+</body>
