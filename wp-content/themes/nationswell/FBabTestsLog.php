4b4d9a64de2df7409e0c71488a15c67fb4c5ffdc
Taylor Johnson <taylor.johnson404@gmail.com>
2014-08-06 13:00:17 -0400

    FB A/B test logs update.

--- wp-content/themes/nationswell/FBabTestsLog.php
+++ wp-content/themes/nationswell/FBabTestsLog.php
@@ -6,6 +6,7 @@ Template Name: FBAB Tester
 
 <!DOCTYPE html>
 <meta charset="utf-8">
+<title>NationSwell FB A/B Testing</title>
 <script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
 <script src="http://d3js.org/d3.v3.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/tabletop.js/1.1.0/tabletop.min.js"></script>
@@ -13,10 +14,16 @@ Template Name: FBAB Tester
 
 <body>
 <div id="topnavspacer"><h1>NationSwell Facebook A/B Testing Results</h1></div>
+</div>
+	<div id="datadrop"></div>
 <div id="topnav">
-    <h1>NationSwell Facebook A/B Testing Results</h1>
+	<h1>NationSwell Facebook A/B Testing Results</h1>
+	<div id="controlrow">
+	<input type="checkbox" id="sscheckbox">Show Non-Significant Results</input>
 </div>
-</div>
-<div id="datadrop"></div>
-<script src="http://nationswell.com/wp-content/themes/nationswell/FBABTestLog/FBabTestDataSort.js" type="text/javascript"></script>
+	</div>
+
+
+	<script src="http://nationswell.com/wp-content/themes/nationswell/FBABTestLog/FBabTestDataSort.js" type="text/javascript"></script>
+
 </body>
