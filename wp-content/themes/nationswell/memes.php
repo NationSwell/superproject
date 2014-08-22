9e7a609b917b2a645df34d082cbed7d911ce53e5
imwm <mwmoretti2@gmail.com>
2014-08-22 13:23:01 -0400

    creates meme maker template

--- wp-content/themes/nationswell/memes.php
+++ wp-content/themes/nationswell/memes.php
@@ -11,8 +11,20 @@ Template Name: mememaker
 <script src="http://d3js.org/d3.v3.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/tabletop.js/1.1.0/tabletop.min.js"></script>
 
+<style type="text/css">
+    body {
+        margin:0px;
+    }
+    #meme-embed {
+        display:inline-block;
+        min-width:100%;
+        min-height:100%;
+    }
+
+</style>
+
 <body>
-<iframe src="http://nsmemes.herokuapp.com/" width="100%" height="100%" frameborder="0" scrolling="no">
+<iframe id="meme-embed" src="http://nsmemes.herokuapp.com/" width="100%" height="100%" frameborder="0" scrolling="no">
 </iframe>
 
 </body>
