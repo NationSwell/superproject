e59f4e58dc15d205fe3f1804478f8852d19fb3fc
Cyril Tsiboulski <ct@cloudred.com>
2016-08-15 18:49:24 -0400

    ifixed wrong field key reference

--- wp-content/themes/nationswell/buddypress/service-opportunities/listing-service-opportunities.php
+++ wp-content/themes/nationswell/buddypress/service-opportunities/listing-service-opportunities.php
@@ -14,8 +14,9 @@ global $bp;
 			if(empty($_REQUEST['location']) && empty($_REQUEST['search_opportunities'])){
 				$_REQUEST['location'] = bp_get_profile_field_data(array('field'=>'Council branch','user_id'=>$bp->loggedin_user->id));
 			}
-			$field_key = "field_576b17d4f5dad";
+			$field_key = "field_578ff22261702";
 			$field = get_field_object($field_key);
+			print_r($field);
 			if( $field ){
 				echo '<form method="get">';
 				echo '<label for="location">'.__('Displaying opportunities in','buddypress').'</label>';
